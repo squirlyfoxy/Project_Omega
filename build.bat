bcf200ce9173757814ad1739c66166d75974e609
squirlyfoxy <public.thesquirlyz97@gmail.com>
2020-09-23 20:24:31 +0200

    Working kpt script

--- build.bat
+++ build.bat
@@ -12,26 +12,30 @@ echo Building kernel:
 echo - Building Stage 1
 wsl $HOME/opt/cross/bin/x86_64-elf-gcc  -Ttext 0x8000 -ffreestanding -mno-red-zone -m64 -c "kernel/kentry.cpp" -o "out/kentry.o"
 
-:: Generating KPT
-echo Generating KPT bin file:
-
-echo - Executing kpt.py
-:: TODO: Call kpt.py
-
-
 echo Linking kernel:
 echo - Linking Stage 1
 wsl $HOME/opt/cross/x86_64-elf/bin/x86_64-elf-ld -T "linker.ld"
+
+:: Generating KPT
+echo Generating KPT bin file:
+
+echo - Executing kpt.py:
+py kpt.py
+
+:: Bulding KPT
+echo - Building KPT
+nasm bootloader/kpt.asm -o out/kpt.o
 
 :: Create system image
 echo Creating system image:
 cd out
 
-echo - Combining KPT and bootloader togheder
+echo - Combining Bootloader and KPT togheder
 :: TODO: Combining KPT & bootloader togheder
+copy /B /Y "bootloader.bin"+"kpt.o" "finalBoot.bin"
 
 :: Combining files togheder togheder
 echo - Combining files togheder
-copy /B /Y "bootloader.bin"+"kernel.bin" "boot.bin"
-del bootloader.bin *.o kernel.bin frsTKernel.tmp
+copy /B /Y "finalBoot.bin"+"kernel.bin" "boot.bin"
+del bootloader.bin *.o kernel.bin frsTKernel.tmp finalBoot.bin kpt.o
 cd ..
--- kpt.py
+++ kpt.py
@@ -1,4 +1,55 @@
 # KPT è uno script python che viene eseguito al build del progetto, non fa parte del SO
-# KPT sta per (Kernel Partition Table), sono 512bytes (situati nel secondo settore) che contengono da quale settore parte il kernel di "Project Omega" e per quanti dura nell'hdd
+# KPT sta per Kernel Partition Table e coincide coi 512bytes situati nel secondo settore che contengono da quale settore parte il kernel di "Project Omega" e per quanti dura nell'hdd
 # Il KPT viene letto e interpretato dal codice situato nel primo settore, non è linguaggio macchina
 # Oltre a questo KPT contiene anche il punto di inizio e il punto di fine della root (questi bytes verranno richiamati dal kernel quando andrà a cercare gli eseguibili principali)
+
+import os, math
+
+# Prendi la dimensione in kBytes del file "./out/kernel.bin"
+kernelPath = "./out/kernel.bin"
+
+def GetKernelKB(path):
+    return os.path.getsize(path)
+
+size = GetKernelKB(kernelPath)
+nBlock = (int)(round(size / 512, 0))
+
+print("Kernel size: ", size, "bytes")
+print("Kernel size in sectors: ", nBlock)
+
+# Ora ho bisogno di una regola per indicizzare il numero di blocchi dentro il KPT in modo che sia univoco e ridondante nel caso in cui il peso del kernel diventi importante
+# In un blocco ci sono 512bytes, noi di un kernel dobbiamo salvare solo quanti blocchi dura visto che ci diamo la regola che inia tutto dal terzo blocco
+# Stessa cosa succederà per la posizione della root visto che ogni blocco contiene l'indice a quello successivo...
+
+# Quindi... mi calcolo quanti saranno i bit necessari per indicizzare nBlock blocchi (log in base 2 di nBlock), questo numero verrà diviso per 8 e avremo il numero di bytes necessari
+
+nBitForHDD = (int)(math.log(nBlock, 2))
+i = 1 # Per il ciclo while
+
+if isinstance(nBitForHDD / 8, float):
+    # Vuol dire che non riesco ad occupare un byte intero, colcoliamo quanto mi resta
+    nBitForHDD = (int)(nBitForHDD)
+    while True:
+        if (nBitForHDD + i) % 8 == 0:
+            nBitForHDD += i
+            break
+        i += 1
+
+print("The kernel will be indexed with: ", (int(nBitForHDD / 8)), " bytes")
+
+# I primi 2 bytes verranno usati per indicare quanti bytes devo leggere dal terzo in poi
+nBlockHex = "0x{:02x}".format(nBlock)
+nLblockHex = "0x{:02x}".format(len((str)(nBlockHex[2:])))
+
+print(nBlockHex, " will be stored from the 3rd byte for a lenght of ", nLblockHex, " bytes, generating table:")
+
+f = open("./bootloader/kpt.asm", "w+") # Crea il file tabella dentro bootloader
+toWrite = "dw 0x" + nBlockHex[2:] + nLblockHex[2:] + "\r\n"
+
+f.write(toWrite)
+
+f.write('a: db "PrOFS KPT table", 0 \r\n')
+f.write("times 512 - ($ - $$) db 0 \r\n")
+f.close()
+
+print("OK")
