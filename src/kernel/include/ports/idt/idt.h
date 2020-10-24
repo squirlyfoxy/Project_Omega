0262384d25f8dcca2d87ca5e42ccd8e45b51da39
squirlyfoxy <public.thesquirlyz97@gmail.com>
2020-10-24 15:51:01 +0200

    Who programmed this?

--- src/kernel/include/ports/idt/idt.h
+++ src/kernel/include/ports/idt/idt.h
@@ -8,22 +8,23 @@
 #include "../../../IDT/Keyboard/KBScanCode.cpp"
 
 void IDTinit();
-extern "C" void isr0Handler();
-extern "C" void isr1Handler();
-extern "C" void isr2Handler();
-extern "C" void isr3Handler();
-extern "C" void isr4Handler();
-extern "C" void isr5Handler();
-extern "C" void isr6Handler();
-extern "C" void isr7Handler();
-extern "C" void isr8Handler();
-extern "C" void isr9Handler();
-extern "C" void isr10Handler();
-extern "C" void isr11Handler();
-extern "C" void isr12Handler();
-extern "C" void isr13Handler();
-extern "C" void isr14Handler();
-extern "C" void isr15Handler();
+void WhaitIRQ(int who);
+extern "C" void irq0Handler();
+extern "C" void irq1Handler();
+extern "C" void irq2Handler();
+extern "C" void irq3Handler();
+extern "C" void irq4Handler();
+extern "C" void irq5Handler();
+extern "C" void irq6Handler();
+extern "C" void irq7Handler();
+extern "C" void irq8Handler();
+extern "C" void irq9Handler();
+extern "C" void irq10Handler();
+extern "C" void irq11Handler();
+extern "C" void irq12Handler();
+extern "C" void irq13Handler();
+extern "C" void irq14Handler();
+extern "C" void irq15Handler();
 
 extern void (*MainKeyboardHandler) (uint_8 scanCode, uint_8 chr);
 
